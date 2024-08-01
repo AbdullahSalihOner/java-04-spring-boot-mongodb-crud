5850c0ef610671d2964a3df0882e3a6affb54b27
Abdullah Salih Öner <riyadlioner00@gmail.com>
2024-08-01 14:53:31 +0300

    Last Update

--- src/main/java/com/salih/model/Tutorial.java
+++ src/main/java/com/salih/model/Tutorial.java
@@ -1,8 +1,11 @@
 package com.salih.model;
 
+
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "tutorials")
 public class Tutorial {
   @Id
